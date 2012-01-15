6f8c4288c613c688ebfdb9b47a6962e27356caa5
karthirVintech <ranjith_cs75@yahoo.co.in>
2012-01-15 12:07:50 +0530

    first commit

---
+++ testing/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Form2.Show()
+    End Sub
+End Class
